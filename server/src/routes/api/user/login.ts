0b6b15df15f4b5a587b0e8d2327cc9de76c089df
Nick Bland <nick.bland@nickbland.dev>
2024-08-25 14:16:29 +1000

    Add in backend login function from JSON file of users

---
+++ server/src/routes/api/user/login.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response, Router } from "express";
+import fs from "fs";
+
+export const login: Router = express.Router(); // Export the login router
+
+login.post("/api/user/login", (req: Request, res: Response) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).send("Invalid request");
+  }
+
+  const receivedData = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+
+  const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
+
+  // Check if the user exists and password is correct
+  const user = users.find(
+    (user: { username: string; password: string }) =>
+      user.username === receivedData.username &&
+      user.password === receivedData.password
+  );
+
+  if (!user) {
+    return res.status(401).send({ valid: false });
+  } else {
+    return res.send({ valid: true });
+  }
+});
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,12 @@ import express, { Router } from "express";
 
 // Import sub-routes here
 import { healthcheck } from "./healthcheck";
-import { test } from "./api/test";
+
+import { login } from "./api/user/login";
 
 export const routes: Router = express.Router(); // Get the router instance
 
 // Use sub-routes here
 routes.use(healthcheck);
-routes.use(test);
+
+routes.use(login);
