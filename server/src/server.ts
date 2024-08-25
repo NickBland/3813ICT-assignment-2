985eaf88b0aee7942eea8a56933c6579ba34a4d0
Nick Bland <nick.bland@nickbland.dev>
2024-08-25 14:36:18 +1000

    Add in CORS
    YES its insecure to allow everything, NO i won't change it yet. CORS is a pain, and Angular freaks out if this isn't put in.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,21 @@
 import express, { Express } from "express";
+import cors from "cors";
 
 const PORT = 8888; // Run server on 8888/8080 for http & 3000/3001 for https
 
 const app: Express = express();
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+const corsMiddleware = cors(corsOptions);
 
 app.use(express.urlencoded({ extended: true })); // Enable URL parsing middleware
 app.use(express.json()); // Enable JSON parsing middleware
 
 ///// DEFINE & USE ROUTES /////
 import { routes } from "./routes/index"; // Gather all sub-routes fron the index file
-app.use("/", routes); // Push them to the root URL
+app.use("/", corsMiddleware, routes); // Push them to the root URL
 
 ///// START SERVER /////
 app.listen(PORT, () => {
