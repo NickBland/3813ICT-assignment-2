6335eee9c4e9a9df70813462bb05415d86e42953
Nick Bland <nick.bland@nickbland.dev>
2024-08-26 18:04:15 +1000

    Prevent refreshing mid-session from messing up state
    Force a refresh of the login state on every refresh in case the session storage is still present. Stops user from being required to login every time.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { UserService } from "./user.service";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, NavbarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'assignment-1';
+export class AppComponent implements OnInit {
+  title = "assignment-1";
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.refreshLoginState();
+  }
 }
