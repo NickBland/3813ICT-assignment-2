fcaba99778a1e5fecf67c286d15203688871ee31
Nick Bland <nick.bland@nickbland.dev>
2024-08-26 16:32:30 +1000

    Add in JWT auth for user endpoints

--- server/src/routes/api/user/users.ts
+++ server/src/routes/api/user/users.ts
@@ -1,11 +1,32 @@
 import express, { Request, Response, Router } from "express";
 import fs from "fs";
 
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
 export const users: Router = express.Router(); // Export the users router
 
-// Get all the users (without password) from the users.json file
+const secret = process.env.JWT_SECRET || "defaultSecret";
 
-users.get("/api/user/users", (_req: Request, res: Response) => {
+// Create a simple verification middleware function for use in these calls
+function verifyToken(req: Request, res: Response, next: () => void) {
+  const token = req.headers.authorization?.split(" ")[1]; // Get auth header, remove "Bearer " from the start
+
+  if (!token) {
+    return res.status(401).send({ message: "Access Denied" });
+  }
+
+  try {
+    const verified = jwt.verify(token, secret);
+    req.body.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Invalid Token", error });
+  }
+}
+
+// Get all the users (without password) from the users.json file
+users.get("/api/user/users", verifyToken, (_req: Request, res: Response) => {
   const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
 
   users.forEach((user?: { password?: string }) => {
@@ -16,17 +37,21 @@ users.get("/api/user/users", (_req: Request, res: Response) => {
 });
 
 // Get a single user by username (without password) from the users.json file
-users.get("/api/user/users/:username", (req: Request, res: Response) => {
-  const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
+users.get(
+  "/api/user/users/:username",
+  verifyToken,
+  (req: Request, res: Response) => {
+    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
 
-  const user = users.find(
-    (user: { username: string }) => user.username === req.params.username
-  );
+    const user = users.find(
+      (user: { username: string }) => user.username === req.params.username
+    );
 
-  if (!user) {
-    return res.status(404).send("User not found");
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    delete user.password; // Remove the password from the response
+    return res.send(user);
   }
-
-  delete user.password; // Remove the password from the response
-  return res.send(user);
-});
+);
