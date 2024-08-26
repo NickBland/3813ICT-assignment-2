515d7a536ba2700e449b8c7d2955dfc5c654d8bf
Nick Bland <nick.bland@nickbland.dev>
2024-08-26 18:18:52 +1000

    Add in error handling for not being logged in

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,19 +1,37 @@
-import { Component, WritableSignal } from "@angular/core";
+import { Component, Signal, OnInit } from "@angular/core";
 import { User } from "../user";
+import { UserService } from "../user.service";
+import { ErrorComponent } from "../error/error.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-profile",
   standalone: true,
-  imports: [],
+  imports: [ErrorComponent, RouterModule],
   templateUrl: "./profile.component.html",
   styleUrl: "./profile.component.scss",
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   // Simple profile view, allowing the user to see their own profile
   // There should also be a pop up form to edit the user's profile
   // This form should have a cancel button and a save button
 
-  // instantiate the user service here
-  user$ = {} as WritableSignal<User>;
-  
+  // insantaite the signals, and error code here
+  user$ = {} as Signal<User>;
+  loggedIn$ = {} as Signal<boolean>;
+  error: Error | null = null;
+
+  constructor(private userService: UserService) {
+    // set the signals up
+    this.user$ = this.userService.user$;
+    this.loggedIn$ = this.userService.loggedIn$;
+  }
+
+  // Determine if the user is logged in, otherwise throw an error
+  ngOnInit() {
+    this.userService.refreshLoginState();
+    if (!this.loggedIn$()) {
+      this.error = new Error("You must be logged in to view this page!");
+    }
+  }
 }
