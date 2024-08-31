d736d8f004e119b4c0d6381d1e8dd509205c805a
Nick Bland <nick.bland@nickbland.dev>
2024-09-01 01:34:12 +1000

    Add in ID -> Name and Name -> ID. Fix up logic error in group impl.

--- src/app/channel.service.ts
+++ src/app/channel.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Channel } from "./channel";
+import { firstValueFrom } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -44,5 +45,63 @@ export class ChannelService {
       `${this.apiURL}/api/channel/${channelId}`,
       channel
     );
+  }
+
+  // if (groupNames.length > 0) {
+  //   const groupIds: number[] = [];
+  //   for (const group of groups) {
+  //     if (groupNames.includes(group.name)) {
+  //       groupIds.push(group.id);
+  //     }
+  //   }
+  //   return groupIds;
+  // } else {
+  //   for (const group of groups) {
+  //     if (group.name === groupName) {
+  //       return group.id;
+  //     }
+  //   }
+  // }
+
+  // Channel Name -> ID
+  async getChannelIdFromName(channelName = "", channelNames: string[] = []) {
+    const channels = await firstValueFrom(this.getChannels(0));
+    if (channelNames.length > 0) {
+      const channelIds: number[] = [];
+      for (const channel of channels) {
+        if (channelNames.includes(channel.name)) {
+          channelIds.push(channel.id as number);
+        }
+      }
+      return channelIds;
+    } else {
+      for (const channel of channels) {
+        if (channel.name === channelName) {
+          return channel.id;
+        }
+      }
+    }
+    return -1;
+  }
+
+  // Channel ID -> Name
+  async getChannelNameFromId(channelId = 0, channelIDs: number[] = []) {
+    const channels = await firstValueFrom(this.getChannels(0));
+    if (channelIDs.length > 0) {
+      const channelNames: string[] = [];
+      for (const channel of channels) {
+        if (channelIDs.includes(channel.id as number)) {
+          channelNames.push(channel.name);
+        }
+      }
+      return channelNames;
+    } else {
+      for (const channel of channels) {
+        if (channel.id === channelId) {
+          return channel.name;
+        }
+      }
+    }
+    return "";
   }
 }
