a21b0bdaba0628afdaeca0872d917fe8d024dc63
Nick Bland <nick.bland@nickbland.dev>
2024-10-09 16:00:51 +1000

    Tests for Login

---
+++ server/test/test-login.spec.ts
@@ -0,0 +1,104 @@
+import { app } from "../src/server";
+
+import { before, after } from "mocha";
+import { use, expect } from "chai";
+import chaiHttp from "chai-http";
+
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+const chai = use(chaiHttp);
+
+describe("Login", () => {
+  let mongoServer: MongoMemoryServer;
+
+  before(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri);
+
+    const userSchema = new mongoose.Schema({
+      username: String,
+      password: String,
+    });
+
+    const User = mongoose.model("User", userSchema);
+
+    const user = new User({
+      username: "super",
+      password: "123",
+    });
+
+    await user.save();
+  });
+
+  after(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+
+  describe("POST Login Correct Password", () => {
+    it("should return a 200 status", (done) => {
+      chai
+        .request(app)
+        .post("/api/user/login")
+        .send({
+          username: "super",
+          password: "123",
+        })
+        .end((err, res) => {
+          expect(res.status).to.be.equal(200);
+          expect(res.body).to.have.property("authToken");
+          expect(res.body).to.not.have.property("password");
+          done();
+        });
+    });
+  });
+
+  describe("POST Login Incorrect Password", () => {
+    it("should return a 401 status", (done) => {
+      chai
+        .request(app)
+        .post("/api/user/login")
+        .send({
+          username: "super",
+          password: "1234",
+        })
+        .end((err, res) => {
+          expect(res.status).to.be.equal(401);
+          done();
+        });
+    });
+  });
+
+  describe("POST Login Incorrect Username", () => {
+    it("should return a 401 status", (done) => {
+      chai
+        .request(app)
+        .post("/api/user/login")
+        .send({
+          username: "superman",
+          password: "123",
+        })
+        .end((err, res) => {
+          expect(res.status).to.be.equal(401);
+          done();
+        });
+    });
+  });
+
+  describe("POST Login No Username", () => {
+    it("should return a 400 status", (done) => {
+      chai
+        .request(app)
+        .post("/api/user/login")
+        .send({
+          password: "123",
+        })
+        .end((err, res) => {
+          expect(res.status).to.be.equal(400);
+          done();
+        });
+    });
+  });
+});
